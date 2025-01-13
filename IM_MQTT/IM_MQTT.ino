e326cc4f6102a0689d9957224452ef2ba6bfadc4
Prasetyo Ade <prasetyoadep123@gmail.com>
2025-01-13 19:59:25 +0700

    Update IM_MQTT.ino

--- IM_MQTT/IM_MQTT.ino
+++ IM_MQTT/IM_MQTT.ino
@@ -2,6 +2,8 @@
 #include <Ticker.h>
 //#include <LiquidCrystal_I2C.h>
 //#include <DHT.h>
+#include <Wire.h>
+#include <BH1750.h>
 #include <ESP8266WiFi.h>
 //#include <WiFi.h>     //esp32
 #include <ESP8266HTTPClient.h>  
@@ -13,21 +15,17 @@
 
 // Password untuk autentikasi OTA
 const char* ota_password = "123";
-void setupOTA();
 String OTA_msg = "Update #2";
 
 // Google script Web_App_URL.
 String Web_App_URL = "https://script.google.com/macros/s/AKfycbz3S04CGjpaPvaXhfWPEMwXwXurooe9cCbCTPkuVKQfBuWraHT5OzGsCln2pL3PFu-r/exec";
 void spreadsheet();
-void readSpreadsheetData();
 int dataSpreadsheet;
 String warning;
 
 //MQTT
 const char* mqtt_server = "broker.mqtt-dashboard.com";
 const int mqtt_port = 1883;
-//const char* mqtt_user = "";     // Username MQTT (opsional)
-//const char* mqtt_password = ""; // Password MQTT (opsional)
 
 const char* topic_temperature = "greenhouse/sensor/temperature";
 const char* topic_humidity = "greenhouse/sensor/humidity";
@@ -49,10 +47,10 @@ const char* topic_kondisi_lampu = "greenhouse/kondisi/lampu";
 const char* topic_warning = "greenhouse/info/warning";
 const char* topic_ota = "greenhouse/info/ota";
 
-char ssid[] = "Prastzy.net"; 
-char pass[] = "123456781";  
+const char* ssid = "Prastzy.net"; 
+const char* pass = "123456781";  
 //const char* ssid ="bebas";
-//const char* password = "akunulisaja";
+//const char* pass = "akunulisaja";
 
 //#define DHTPIN 4 
 //#define DHTTYPE DHT22 
@@ -65,11 +63,13 @@ char pass[] = "123456781";
 //DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
+BH1750 lightMeter;
 //RTC_DS1307 rtc;
 int setWaktu1 [] = {4,0,0}; //jam, menit, detik
 int setWaktu2 [] = {5,0,0};
 int setWaktu3 [] = {18,0,0};
 
+const int delay_sprayer = 5000; 
 int lightValue;
 float temperature, humidity;
 float set_temperature = 30;
@@ -79,6 +79,10 @@ long lama_cahaya, lama_lampu, waktu_lampu;
 #define EEPROM_SIZE 12 
 String kondisi_kipas = "Padam", kondisi_sprayer = "Padam", kondisi_lampu = "Padam";
 bool jam4, jam5_18, jam18_4, status_kipas, status_sprayer, status_lampu;
+bool modeotomatis = false;
+String statusSistem = "Manual";
+float kalibrasi_suhu = 0.67, kalibrasi_kelembaban = -13.1, kalibrasi_lux = 3.96 ;
+String msglampu1, msglampu2;
 
 void baca_RTC();
 void setup_wifi();
@@ -99,7 +103,15 @@ void setup() {
   //pinMode(LDR_PIN, INPUT);
 
   setup_wifi();
-  //dht.begin();
+  /*
+  dht.begin();
+  Wire.begin();
+  if (!lightMeter.begin()) {
+    Serial.println(F("Gagal menginisialisasi sensor BH1750!"));
+    Serial.println(F("Periksa koneksi kabel."));
+    while (1); // Berhenti jika inisialisasi gagal
+  }
+  */
   //lcd.init();   
   /*      
   lcd.backlight();    
@@ -110,7 +122,9 @@ void setup() {
   delay(1000);
   lcd.clear();
   */
-  setupOTA(); 
+  ArduinoOTA.setHostname("esp8266 - Iklim Mikro");
+  ArduinoOTA.setPassword(ota_password);
+  ArduinoOTA.begin();  
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
@@ -124,8 +138,6 @@ void setup() {
   Serial.println("Nilai Lama Cahaya dari EEPROM: " + String(lama_cahaya));
   Serial.println("Nilai Lama Lampu dari EEPROM: " + String(lama_lampu));
   Serial.println("Nilai Waktu Lampu dari EEPROM: " + String(waktu_lampu));
-  
-
   /*
    if (! rtc.begin()) {
     Serial.println("RTC tidak ditemukan");
@@ -137,25 +149,19 @@ void setup() {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Set waktu sesuai waktu kompilasi
   }
   */
-
   task1.attach(3, monitoring);
   task2.attach(3, kontrol_kipas);
   task3.attach(3, kontrol_lampu);
 }
 
 void loop() { 
-  if (!client.connected()) {
-          reconnect();
+  if(!client.connected()) {
+     reconnect();
   }
   client.loop();
   kontrol_sprayer();
   
   ArduinoOTA.handle();
-  if (status_kipas && status_sprayer){
-    spreadsheet();
-    readSpreadsheetData();
-    delay(5000);
-  }
 }
 
 //Task monitoring
@@ -164,7 +170,13 @@ void monitoring() {
     //baca sensor
     temperature = dht.readTemperature();
     humidity = dht.readHumidity();
-    lightValue = analogRead(LDR_PIN);
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println(F("Gagal membaca dari sensor DHT22!"));
+      return;
+    }
+    temperature += 0.67; // Tambahkan 0.67 ke suhu
+    humidity -= 13.1;     // Kurangi 5.7 dari kelembapan
+    float lightValue = lightMeter.readLightLevel();
     baca_RTC();
   
     //serial monitor
@@ -173,9 +185,15 @@ void monitoring() {
     Serial.print("Cahaya : "); Serial.println(lightValue);
     Serial.println("");
 */
+    //=======================================
     temperature = random(25, 36);
     humidity = random(70, 91);
     lightValue = random(500, 1000);
+    
+    temperature +=  kalibrasi_suhu; //0.67; 
+    humidity += kalibrasi_kelembaban; //-13.1; 
+    lightValue = lightValue * kalibrasi_lux;    
+    //=======================================
     //Print LCD
     /*
     lcd.setCursor(0, 1);
@@ -193,17 +211,11 @@ void monitoring() {
     client.publish(topic_kondisi_kipas, kondisi_kipas.c_str());
     client.publish(topic_kondisi_sprayer, kondisi_sprayer.c_str());
     client.publish(topic_kondisi_lampu, kondisi_lampu.c_str());
-
-    if (dataSpreadsheet > 10000){
-      warning = "Data sudah terlalu banyak";
-    }else{
-      warning = String (dataSpreadsheet);  
-    }
-    client.publish(topic_warning, warning.c_str());
 }
 
 //Task pengkondisian kipas
 void kontrol_kipas(){
+  if(modeotomatis){
     if (temperature > set_temperature){
       digitalWrite (relay_kipas, HIGH);
       kondisi_kipas = "Menyala";
@@ -217,51 +229,53 @@ void kontrol_kipas(){
         kondisi_kipas = "Padam";
       }
     }
+  }
 }
 
 //Task pengkondisian sprayer
-void kontrol_sprayer(){
-  unsigned long previousMillisSprayer = 0;  // Waktu sebelumnya
-  const unsigned long intervalSprayer = 5000;  // Interval waktu dalam ms (5 detik)
-  bool sprayerActive = false;  // Status aktif sprayer
-  // Mengecek apakah kelembaban di bawah nilai setpoint
-  if (humidity < set_humidity) {
-    if (!sprayerActive) {
-      digitalWrite(relay_sprayer, HIGH);
-      kondisi_sprayer = "Menyala";
-      previousMillisSprayer = millis();  // Catat waktu saat sprayer diaktifkan
-      sprayerActive = true;
-    }
+void kontrol_sprayer() {
+  if(modeotomatis){
+    static unsigned long previousMillisSprayer = 0;  // Waktu sebelumnya
+    static bool sprayerActive = false;              // Status sprayer   
+    static bool firstActivation = true; 
+    
+    // Kondisi ketika kelembapan lebih kecil dari setpoint
+    if (humidity < set_humidity) {
+        if (!sprayerActive && firstActivation) {
+            digitalWrite(relay_sprayer, HIGH);
+            kondisi_sprayer = "Menyala";
+            previousMillisSprayer = millis();  // Catat waktu saat sprayer diaktifkan
+            sprayerActive = true;             // Tandai sprayer sebagai aktif
+            firstActivation = false;
+        }
 
-    // Matikan sprayer setelah 5 detik
-    if (millis() - previousMillisSprayer >= intervalSprayer) {
-      digitalWrite(relay_sprayer, LOW);
-      kondisi_sprayer = "Padam";
-      sprayerActive = false;
+        // Matikan sprayer setelah durasi tertentu
+        if (millis() - previousMillisSprayer >= delay_sprayer) {
+            digitalWrite(relay_sprayer, LOW);
+            kondisi_sprayer = "Padam";
+            sprayerActive = false;            // Tandai sprayer sebagai tidak aktif
+            firstActivation = false;
+        }
     }
-  }
-  // Jika kelembaban >= setpoint
-  else if (humidity >= set_humidity) {
-    if (status_sprayer) {
-      if (!sprayerActive) {
-        digitalWrite(relay_sprayer, HIGH);
-        kondisi_sprayer = "Menyala";
-        previousMillisSprayer = millis();
-        sprayerActive = true;
-      }
-
-      if (millis() - previousMillisSprayer >= intervalSprayer) {
-        digitalWrite(relay_sprayer, LOW);
-        kondisi_sprayer = "Padam";
-        sprayerActive = false;
-      }
-    }else {
-      digitalWrite(relay_sprayer, LOW);
-      kondisi_sprayer = "Padam";
-      sprayerActive = false;
+    // Kondisi ketika kelembapan >= setpoint
+    else if (humidity >= set_humidity) {
+        if (status_sprayer) {
+            // Nyalakan sprayer jika status_sprayer aktif
+            digitalWrite(relay_sprayer, HIGH);
+            kondisi_sprayer = "Menyala";
+            firstActivation = true;
+        } else {
+            // Matikan sprayer jika status_sprayer tidak aktif
+            digitalWrite(relay_sprayer, LOW);
+            kondisi_sprayer = "Padam";
+            sprayerActive = false;  // Pastikan status sprayer juga tidak aktif
+            firstActivation = true;
+        }
     }
   }
 }
+
+
 
 
 //task baca cahaya
@@ -285,7 +299,7 @@ void kontrol_lampu () {
             Serial.print("lama cahaya :"); Serial.println(lama_cahaya);
           }else {
             lama_cahaya += 3;
-            Serial.print("lama cahaya :"); Serial.println(lama_cahaya);
+            Serial.print("lama cahaya  :"); Serial.println(lama_cahaya);
           }
         }
         else {
@@ -294,6 +308,7 @@ void kontrol_lampu () {
         lama_lampu = (100 - lama_cahaya);
         Serial.print("lama lampu : "); Serial.println(lama_lampu);
     }
+  if(modeotomatis){
     //jam 18 sampai jam 3 , menyalakan lampu
     //if (now.hour() > setWaktu3[0] && now.hour() < setWaktu1[0]){
     if (jam18_4 == true) {
@@ -314,6 +329,11 @@ void kontrol_lampu () {
         }
       }
     }
+  }
+  msglampu1 = "=> " + String(lama_lampu) + " detik";
+  msglampu2 = "Lampu sudah menyala selama : " + String(waktu_lampu) + " detik";
+  client.publish("greenhouse/info/lamalampu", msglampu1.c_str());
+  client.publish("greenhouse/info/waktulampu", msglampu2.c_str());
     // Simpan nilai terbaru ke EEPROM
     writeLong(0, lama_cahaya); // Simpan di offset 0
     writeLong(4, waktu_lampu); // Simpan di offset 4
@@ -347,14 +367,31 @@ void callback(char *topic, byte *payload, unsigned int length) {
   msg[length] = '\0';  
   Serial.println(msg);
 
+  // Saklar Mode Otomatis 
+  if (!strcmp(topic, "greenhouse/kontrol/modeotomatis")) {
+    if (!strncmp(msg, "on", length)) {
+      modeotomatis = true;
+      statusSistem = "Otomatis";
+      client.publish("greenhouse/info/modeotomatis", statusSistem.c_str()); 
+    } else if (!strncmp(msg, "off", length)) {
+      modeotomatis = false;
+      statusSistem = "Manual";
+      client.publish("greenhouse/info/modeotomatis", statusSistem.c_str());
+    }
+  }
+
   // kontrol kipas
   if (!strcmp(topic, topic_kontrol_kipas)) {
     if (!strncmp(msg, "on", length)) {
       status_kipas = true;
       Serial.println("Saklar Kipas ON");
+      digitalWrite (relay_kipas, HIGH);
+      kondisi_kipas = "Menyala";
     } else if (!strncmp(msg, "off", length)) {
       status_kipas = false;
       Serial.println("Saklar Kipas OFF");
+      digitalWrite (relay_kipas, LOW);
+      kondisi_kipas = "Padam";
     }
   }
 
@@ -363,9 +400,13 @@ void callback(char *topic, byte *payload, unsigned int length) {
     if (!strncmp(msg, "on", length)) {
       status_sprayer = true;
       Serial.println("Saklar Sprayer ON");
+      digitalWrite (relay_sprayer, HIGH);
+      kondisi_sprayer = "Menyala";
     } else if (!strncmp(msg, "off", length)) {
       status_sprayer = false;
       Serial.println("Saklar Sprayer OFF");
+      digitalWrite (relay_sprayer, LOW);
+      kondisi_sprayer = "Padam";
     }
   }
 
@@ -376,14 +417,45 @@ void callback(char *topic, byte *payload, unsigned int length) {
       kondisi_lampu = "Menyala";
       status_lampu = true;
       Serial.println("Saklar Lampu ON");
+      digitalWrite(relay_lampu, HIGH);
     } else if (!strncmp(msg, "off", length)) {
       digitalWrite(relay_lampu, LOW);
       kondisi_lampu = "Padam";
       status_lampu = false;
       Serial.println("Saklar Lampu OFF");
+      digitalWrite(relay_lampu, LOW);
     }
   }
-
+  //===================================================
+  // Ambil data Spreadsheet
+  if (!strcmp(topic, "greenhouse/kontrol/ambildata")) {
+    if (!strncmp(msg, "on", length)) {
+      Serial.println("Mengambil data untuk spreadsheet");
+      spreadsheet();
+    }
+  }
+  // KALIBRASI SUHU
+  if (!strcmp(topic, "greenhouse/kontrol/kalibrasisuhu")) {
+    kalibrasi_suhu = atof(msg); 
+    Serial.print("Set kalibrasi Suhu menjadi : ");
+    Serial.println(kalibrasi_suhu);
+    client.publish("greenhouse/info/kalibrasisuhu", String(kalibrasi_suhu).c_str());
+  }
+  // KALIBRASI Kelembaban
+  if (!strcmp(topic, "greenhouse/kontrol/kalibrasikelembaban")) {
+    kalibrasi_kelembaban = atof(msg); 
+    Serial.print("Set kalibrasi Kelembaban menjadi : ");
+    Serial.println(kalibrasi_kelembaban);
+    client.publish("greenhouse/info/kalibrasikelembaban", String(kalibrasi_kelembaban).c_str());
+  }
+  // KALIBRASI Lux
+  if (!strcmp(topic, "greenhouse/kontrol/kalibrasilux")) {
+    kalibrasi_lux = atof(msg); 
+    Serial.print("Set kalibrasi Lux menjadi : ");
+    Serial.println(kalibrasi_lux);
+    client.publish("greenhouse/info/kalibrasilux", String(kalibrasi_lux).c_str());
+  }
+  //=================================================================
   // Set suhu 
   if (!strcmp(topic, topic_set_temperature)) {
     set_temperature = atof(msg); 
@@ -404,7 +476,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Set Light menjadi : ");
     Serial.println(set_light);
   }
-
+  //=============================================================
   // jam 4
   if (!strcmp(topic, topic_test)) {
     if (!strncmp(msg, "on1", length)) {
@@ -417,7 +489,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
       lama_lampu = 100;
     } else if (!strncmp(msg, "off1", length)) {
       jam4 = false;
-      Serial.println("jam 4 0ff");
+      Serial.println("jam 4 off");
     }
   }
 
@@ -444,21 +516,6 @@ void callback(char *topic, byte *payload, unsigned int length) {
   }
 }
 
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Menghubungkan ke ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Terhubung ke WiFi dengan IP: ");
-  Serial.println(WiFi.localIP());
-}
-
 void reconnect() {
   while (!client.connected()) {
     Serial.println("Attempting MQTT connection...");
@@ -476,6 +533,11 @@ void reconnect() {
       client.subscribe(topic_set_temperature);
       client.subscribe(topic_set_humidity);
       client.subscribe(topic_set_light);
+      client.subscribe("greenhouse/kontrol/modeotomatis");
+      client.subscribe("greenhouse/kontrol/ambildata");
+      client.subscribe("greenhouse/kontrol/kalibrasisuhu");
+      client.subscribe("greenhouse/kontrol/kalibrasikelembaban");
+      client.subscribe("greenhouse/kontrol/kalibrasilux");
       client.subscribe(topic_test);
 
     } else {
@@ -485,6 +547,21 @@ void reconnect() {
       delay(3000);
     }
   }
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Menghubungkan ke ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Terhubung ke WiFi dengan IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void baca_RTC(){
@@ -522,124 +599,47 @@ void baca_RTC(){
   */
 }
 
-void setupOTA() {
-    ArduinoOTA.setHostname("esp8266 - Iklim Mikro");
-    ArduinoOTA.setPassword(ota_password);
-
-    ArduinoOTA.onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH) {
-            type = "sketch";
-        } else { // U_SPIFFS
-            type = "filesystem";
-        }
-        // Notifikasi OTA dimulai
-        Serial.println("Start updating " + type);
-    });
-
-    ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd");
-    });
-
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress * 100) / total);
-    });
-
-    ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) {
-            Serial.println("Auth Failed");
-        } else if (error == OTA_BEGIN_ERROR) {
-            Serial.println("Begin Failed");
-        } else if (error == OTA_CONNECT_ERROR) {
-            Serial.println("Connect Failed");
-        } else if (error == OTA_RECEIVE_ERROR) {
-            Serial.println("Receive Failed");
-        } else if (error == OTA_END_ERROR) {
-            Serial.println("End Failed");
-        }
-    });
-
-    ArduinoOTA.begin();
-    Serial.println("OTA ready with authentication enabled");
-}
-
 void spreadsheet(){
-  // This condition is the condition for sending or writing data to Google Sheets.
   if (WiFi.status() == WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, LOW);
-
-    // Create a URL for sending or writing data to Google Sheets.
     String Send_Data_URL = Web_App_URL + "?sts=write";
     Send_Data_URL += "&lightValue=" + String (lightValue);
     Send_Data_URL += "&temperature=" + String(temperature);
     Send_Data_URL += "&humidity=" + String(humidity);
     Send_Data_URL += "&kipas=" + kondisi_kipas;
     Send_Data_URL += "&sprayer=" + kondisi_sprayer;
-
     Serial.println();
     Serial.println("-------------");
     Serial.println("Send data to Google Spreadsheet...");
     Serial.print("URL : ");
     Serial.println(Send_Data_URL);
 
-      // Initialize HTTPClient as "http".
-      WiFiClientSecure client;
-      client.setInsecure(); // Nonaktifkan validasi SSL
-      HTTPClient http;
-  
-      // HTTP GET Request using the new API
-      http.begin(client, Send_Data_URL);  // Use client and URL
-      http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-  
-      // Gets the HTTP status code.
-      int httpCode = http.GET(); 
-      Serial.print("HTTP Status Code : ");
-      Serial.println(httpCode);
-  
-      // Getting response from google sheets.
-      String payload;
-      if (httpCode > 0) {
-        payload = http.getString();
-        Serial.println("Payload : " + payload);    
-      }
-      
-      http.end();
-    
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("-------------");
-  }
-}
-
-void readSpreadsheetData() {
-  if (WiFi.status() == WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, LOW);
-
-    String Read_Data_URL = Web_App_URL + "?sts=read";
-    Serial.println();
-    Serial.println("-------------");
-    Serial.println("Read data from Google Spreadsheet...");
-    Serial.print("URL : ");
-    Serial.println(Read_Data_URL);
-
     WiFiClientSecure client;
-    client.setInsecure();
+    client.setInsecure(); 
     HTTPClient http;
-    http.begin(client, Read_Data_URL);
+    http.begin(client, Send_Data_URL); 
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS); 
+    int httpCode = http.GET(); 
+    http.end();
+    
+    String Read_Data_URL = Web_App_URL + "?sts=read";
+    http.begin(client, Read_Data_URL); 
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-
-    int httpCode = http.GET();
+    httpCode = http.GET();
     Serial.print("HTTP Status Code : ");
     Serial.println(httpCode);
-
     if (httpCode > 0) {
       String payload = http.getString();
-      Serial.println("Payload : " + payload);
-      dataSpreadsheet = payload.toInt();  // Convert payload to integer
+      Serial.println("Data ke : " + payload);
+      dataSpreadsheet = payload.toInt(); 
     }
-    
     http.end();
-    digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("-------------");
   }
+  // Sistem Peringatan data SpreadSheet
+  if (dataSpreadsheet > 1000){
+    warning = "Data sudah terlalu banyak";
+  }else{
+    warning = String (dataSpreadsheet);  
+  }
+  client.publish(topic_warning, warning.c_str());
 }
